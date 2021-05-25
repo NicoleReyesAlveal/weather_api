8f0374bf75fbb6c44a905416cdab808f1f59e419
nicole <nicole.reyes.alveal@gmail.com>
2021-05-25 12:46:22 -0400

    change variable names

--- app.js
+++ app.js
@@ -11,20 +11,26 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('root endpoint')
 })
 
 app.post('/', (req, res) => {
     console.log(req.body);
     const city = req.body.city;
-    const appId = process.env.OPEN_WEATHER_API_KEY;
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + appId + "&units=metric";
+    const app_id = process.env.OPEN_WEATHER_API_KEY;
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + app_id + "&units=metric";
     https.get(url, (response) => {
         if( response.statusCode === 200) {
             response.on("data", (data) => {
                 const weatherData = JSON.parse(data);
                 console.log(weatherData);
-                res.send({city: weatherData.name, country: weatherData.sys.country, temp: weatherData.main.temp, temp_min: weatherData.main.temp_min, temp_max: weatherData.main.temp_max});
+                res.send({
+                    city: weatherData.name,
+                    country: weatherData.sys.country,
+                    temp: weatherData.main.temp,
+                    temp_min: weatherData.main.temp_min,
+                    temp_max: weatherData.main.temp_max
+                });
             })
         } else {
             res.status(404).send('City not found')
