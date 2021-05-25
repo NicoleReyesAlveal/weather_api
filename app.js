b92fc2e3f1369e03b23737f8dc7d0f71302ef59b
nicole <nicole.reyes.alveal@gmail.com>
2021-05-25 19:08:28 -0400

    add response-time module

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const responseTime = require('response-time');
 const cityRouter = require('./routers/city-router.js');
 
 const app = express()
@@ -11,6 +12,7 @@ require('dotenv').config()
 //Middlewares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(responseTime());
 
 //MongoDB conn string
 const uri = 'mongodb://mongoadmin:pass@localhost:27888/?authSource=admin';
