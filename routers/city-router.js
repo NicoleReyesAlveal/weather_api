136acb2410b17b6c3ccf168f2d8cf425a38e2c24
nicole <nicole.reyes.alveal@gmail.com>
2021-05-25 22:06:45 -0400

    wip: fix saving only when city is new

--- routers/city-router.js
+++ routers/city-router.js
@@ -19,36 +19,42 @@ app.post('/city', async (req, res) => {
 })
 
 app.post('/', async (req, res) => {
-    console.log(req.body.city);
     const city = req.body.city;
     const app_id = process.env.OPEN_WEATHER_API_KEY;
     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + app_id + "&units=metric";
-    const cityName = await cityModel.findOne({city: req.body.city}).exec();
-    console.log('const cityName: ' + cityName + ' ----');
-    if (cityName === null) {
-          https.get(url, (response) => {
-          response.on("data", (data) => {
-          const weatherData = JSON.parse(data);
-          const city = new cityModel({
+    //get city by name
+    const cityName = await cityModel.findOne({city: req.body.city});
+    //if city does not exist, then connect to the API
+    //and persist the new city in mongoDB
+    //TODO: restringir a guardar solo cuando no existe
+    try {
+        if (!cityName) {
+              https.get(url, (response) => {
+                response.on("data", (data) => {
+                    const weatherData = JSON.parse(data);
+                    const city = new cityModel({
+                            city: weatherData.name,
+                            country: weatherData.sys.country,
+                            temp: weatherData.main.temp,
+                            temp_min: weatherData.main.temp_min,
+                            temp_max: weatherData.main.temp_max
+                            })
+                    city.save();
+                    console.log('City saved to DB')
+                    return res.status(201).send({
                         city: weatherData.name,
                         country: weatherData.sys.country,
                         temp: weatherData.main.temp,
                         temp_min: weatherData.main.temp_min,
                         temp_max: weatherData.main.temp_max
-                        })
-          city.save();
-          console.log('City saved to DB')
-          res.send({
-                    city: weatherData.name,
-                    country: weatherData.sys.country,
-                    temp: weatherData.main.temp,
-                    temp_min: weatherData.main.temp_min,
-                    temp_max: weatherData.main.temp_max
-          });
-          })
-       })
-    } else {
-        res.status(200).send(cityName);
+                    });
+              })
+           })
+        } else {
+            res.status(200).send(cityName);
+        }
+    } catch (error) {
+        res.status(500).send(error);
     }
  })
 
