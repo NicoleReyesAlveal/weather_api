36339cad4f4f37f08ef3f6c7a8af2a218ab09919
nicole <nicole.reyes.alveal@gmail.com>
2021-05-25 21:40:06 -0400

    bring data from api if it does not exist in db, otherwise persist it

--- routers/city-router.js
+++ routers/city-router.js
@@ -18,28 +18,38 @@ app.post('/city', async (req, res) => {
     }
 })
 
-app.post('/', (req, res) => {
-    console.log(req.body);
+app.post('/', async (req, res) => {
+    console.log(req.body.city);
     const city = req.body.city;
     const app_id = process.env.OPEN_WEATHER_API_KEY;
     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + app_id + "&units=metric";
-    https.get(url, (response) => {
-        if( response.statusCode === 200) {
-            response.on("data", (data) => {
-                const weatherData = JSON.parse(data);
-                console.log(weatherData);
-                res.send({
+    const cityName = await cityModel.findOne({city: req.body.city}).exec();
+    console.log('const cityName: ' + cityName + ' ----');
+    if (cityName === null) {
+          https.get(url, (response) => {
+          response.on("data", (data) => {
+          const weatherData = JSON.parse(data);
+          const city = new cityModel({
+                        city: weatherData.name,
+                        country: weatherData.sys.country,
+                        temp: weatherData.main.temp,
+                        temp_min: weatherData.main.temp_min,
+                        temp_max: weatherData.main.temp_max
+                        })
+          city.save();
+          console.log('City saved to DB')
+          res.send({
                     city: weatherData.name,
                     country: weatherData.sys.country,
                     temp: weatherData.main.temp,
                     temp_min: weatherData.main.temp_min,
                     temp_max: weatherData.main.temp_max
-                });
-            })
-        } else {
-            res.status(404).send('City not found')
-        }
-    })
+          });
+          })
+       })
+    } else {
+        res.status(200).send(cityName);
+    }
  })
 
  module.exports = app;
